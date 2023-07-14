9e6ba644f7aabe5eb4c1659712f899813c057b20
Nathalia Trazzi <81270839+miucciaknows@users.noreply.github.com>
2023-07-14 14:29:00 -0300

    Update

---
+++ webpage/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import React from 'react';
+
+
+const App = () => {
+  const laboratoryName = 'MEDICINE LEAFLETS'; 
+
+  return (
+    <div className="container">
+      <h1 className="title">{laboratoryName}</h1>
+      <h2 className="tittle-below"> BETTER HEALTH, BETTER LIFE </h2>
+    </div>
+  );
+};
+
+export default App;
